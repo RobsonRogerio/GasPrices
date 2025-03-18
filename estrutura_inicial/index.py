31623171cb51324c381dbed68078afcced147984
Robson Pinheiro <robson.rogerio@gmail.com>
2025-03-18 11:54:53 -0300

    callback max min linha 1

--- estrutura_inicial/index.py
+++ estrutura_inicial/index.py
@@ -24,6 +24,20 @@ url_theme2 = dbc.themes.VAPOR
 
 tab_card = {'height': '100%'}
 
+main_config = {
+    'hovermode': 'x unified',
+    'legend': {
+        'yanchor': 'top',
+        'y': 0.9,
+        'xanchor': 'left',
+        'x': 0.1,
+        'title': {'text': None},
+        'font': {'color': 'white'},
+        'bgcolor': 'rgba(0,0,0,0.5)'
+    },
+    'margin': {'l':0, 'r':0, 't':10, 'b':0}
+}
+
 
 # ===== Reading n cleaning File ====== #
 df_main = pd.read_csv("data_gas.csv")
@@ -248,6 +262,37 @@ app.layout = dbc.Container(children=[
 
 
 # ======== Callbacks ========== #
+
+# callback max min
+@app.callback(
+    Output('static-maxmmin', 'figure'),
+    Input('dataset', 'data'),
+    Input(ThemeSwitchAIO.ids.switch('theme'), 'value')
+)
+def func(data, toggle):
+    template = template_theme1 if toggle else template_theme2
+
+    dff = pd.DataFrame(df_store)
+    max = dff.groupby('ANO')['VALOR REVENDA (R$/L)'].max()
+    min = dff.groupby('ANO')['VALOR REVENDA (R$/L)'].min()
+
+    final_df = pd.concat([max, min], axis=1)
+    final_df.columns = ['Máximo', 'Mínimo']
+
+    fig = px.line(final_df, x=final_df.index, y=final_df.columns, template=template)
+
+    # updates
+    fig.update_layout(main_config, height=150, xaxis_title=None, yaxis_title=None)
+
+    return fig
+
+# callback indicator 2
+
+
+
+
+
+
 
 
 # Run server
